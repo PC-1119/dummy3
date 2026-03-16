bbf8095ee2625e27815b3ff589e088cadfb8edf9
PC-1119 <chevvaprathyusha9550@gmail.com>
2026-03-16 23:06:42 +0530

    Update renovate-config.js

--- .github/ci/renovate-config.js
+++ .github/ci/renovate-config.js
@@ -1,10 +1,8 @@
 module.exports = {
   hostRules: [
     {
-      hostType: "ecr",
+      hostType: "docker",
       matchHost: "819004394028.dkr.ecr.us-east-1.amazonaws.com",
-      username: process.env.RENOVATE_AWS_ACCESS_KEY_ID,
-      password: process.env.RENOVATE_AWS_SECRET_ACCESS_KEY
     }
   ],
 
