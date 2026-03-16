8ef359ba9ee445c519ae3ed09c9d07f1446c7154
PC-1119 <chevvaprathyusha9550@gmail.com>
2026-03-16 22:16:05 +0530

    Update renovate-config.js

--- .github/ci/renovate-config.js
+++ .github/ci/renovate-config.js
@@ -1,20 +1,20 @@
 module.exports = {
   hostRules: [
     {
-      hostType: 'docker', // tells Renovate this is a Docker registry
-      matchHost: '819004394028.dkr.ecr.us-east-1.amazonaws.com',
+      hostType: "docker",
+      matchHost: "819004394028.dkr.ecr.us-east-1.amazonaws.com",
       username: process.env.RENOVATE_AWS_ACCESS_KEY_ID,
-      password: process.env.RENOVATE_AWS_SECRET_ACCESS_KEY,
-      token: process.env.RENOVATE_AWS_SESSION_TOKEN,
-    },
+      password: process.env.RENOVATE_AWS_SECRET_ACCESS_KEY
+    }
   ],
+
   packageRules: [
     {
-      matchDatasources: ['docker'],
+      matchDatasources: ["docker"],
       matchPackageNames: [
-        '819004394028.dkr.ecr.us-east-1.amazonaws.com/pythonbaseimage'
+        "819004394028.dkr.ecr.us-east-1.amazonaws.com/pythonbaseimage"
       ],
-      versioning: 'docker',
-    },
-  ],
+      versioning: "docker"
+    }
+  ]
 };
